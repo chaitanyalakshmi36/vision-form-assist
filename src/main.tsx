17d50630bb3a3ca31092f3106bd96061aa65714f
Chaitanya Lakshmi <chaitanyalakshmimotamarri@gmail.com>
2025-12-28 18:06:38 +0530

    Fix white page issue

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <HashRouter>
-    <App />
-  </HashRouter>
-);
+const root = document.getElementById("root");
+
+if (!root) {
+  throw new Error("Root element not found");
+}
+
+createRoot(root).render(<App />);
