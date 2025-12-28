878bc20a3866087c7c55aa13667c54b891ea8a19
Chaitanya Lakshmi <chaitanyalakshmimotamarri@gmail.com>
2025-12-28 15:51:22 +0530

    Fix router blank screen issue

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
-import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<HashRouter><App/></HashRouter>);
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
